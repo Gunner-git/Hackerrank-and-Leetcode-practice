4fbc917c3cd5fea4ccd9ba9853ff3d7774222f07
Gunner-git <gurnoor2005ronaldo@gmail.com>
2023-06-16 16:57:20 +0530

    Check If It Is a Straight Line added

--- Leetcode/Check If It Is a Straight Line.py
+++ Leetcode/Check If It Is a Straight Line.py
@@ -2,15 +2,20 @@ class Solution:
     def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
         flag = True
 
+        def slopeFunction(x1,y1,x2,y2):
+            if (x2-x1) == 0:
+                slope = "infinity"
+            else:
+                slope = ((y2-y1)/(x2-x1))
+            
+            return slope
+
         x1 = coordinates[0][0]
         y1 = coordinates[0][1]
         x2 = coordinates[1][0]
         y2 = coordinates[1][1]
 
-        if (x2-x1) == 0:
-            slope = "infinity"
-        else:
-            slope = ((y2-y1)/(x2-x1))
+        slope = slopeFunction(x1,y1,x2,y2)
 
         for i in range(0,len(coordinates)-1):
             x1 = coordinates[i][0]
@@ -18,10 +23,7 @@ class Solution:
             x2 = coordinates[i+1][0]
             y2 = coordinates[i+1][1]
 
-            if (x2-x1) == 0:
-                thisSlope = "infinity"
-            else:
-                thisSlope = ((y2-y1)/(x2-x1))
+            thisSlope = slopeFunction(x1,y1,x2,y2)
 
             if thisSlope != slope:
                 flag = False
